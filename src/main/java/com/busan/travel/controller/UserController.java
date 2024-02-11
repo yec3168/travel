b6f872206f76afd2b389ca7de30873905b2bfb67
yec3168 <yec3168@naver.com>
2024-02-11 18:59:32 +0900

    수정

--- src/main/java/com/busan/travel/controller/UserController.java
+++ src/main/java/com/busan/travel/controller/UserController.java
@@ -2,7 +2,6 @@ package com.busan.travel.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,7 +10,12 @@ public class UserController {
 
     @GetMapping("/new")
     public String getNewUser(){
-        return "user/UserForm";
+        return "user/UserLogin";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "user/UserLogin";
     }
 
 
