20a17fccc4892e75aa4e6a5e4626c67325004478
yec3168 <yec3168@naver.com>
2024-02-13 22:27:13 +0900

    수정

---
+++ src/main/java/com/busan/travel/entity/Board.java
@@ -0,0 +1,26 @@
+package com.busan.travel.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    private String subject;
+
+    private String content;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime updateDate;
+
+    private int view; // 조회수
+}
