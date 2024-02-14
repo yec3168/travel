99628b8bc77522fd958c699e4deac81adec454c9
yec3168 <yec3168@naver.com>
2024-02-14 23:20:35 +0900

    failhandler 생성

--- src/main/java/com/busan/travel/entity/User.java
+++ src/main/java/com/busan/travel/entity/User.java
@@ -5,8 +5,6 @@ import com.busan.travel.status.Gender;
 import com.busan.travel.status.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
@@ -15,6 +13,7 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 @Getter
+@Setter
 public class User {
 
     @Id
@@ -22,13 +21,12 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
-    private String email;
-
-    private String password;
-
     private String name;
 
     private String nickName;
+    private String email;
+
+    private String password;
 
     private String address;
 
@@ -41,33 +39,46 @@ public class User {
     private UserRole userRole;
 
 
-    @Builder
-    private User(String email, String password ,String name,
-                 String nickName, String address, Gender gender,
-                 UserRole userRole, LocalDateTime createDate){
-        this.email =email;
-        this.password = password;
-        this.name = name;
-        this.nickName =nickName;
-        this.address =address;
-        this.gender =gender;
-        this.userRole = userRole;
-        this.createDate= createDate;
-    }
+//    @Builder
+//    private User(String email, String password ,String name,
+//                 String nickName, String address, Gender gender,
+//                 UserRole userRole, LocalDateTime createDate){
+//        this.email =email;
+//        this.password = password;
+//        this.name = name;
+//        this.nickName =nickName;
+//        this.address =address;
+//        this.gender =gender;
+//        this.userRole = userRole;
+//        this.createDate= createDate;
+//    }
 
-    public static User createUser(UserFormDto userFormDto, PasswordEncoder passwordEncoder){
-        User user = User.builder()
-                .name(userFormDto.getName())
-                .nickName(userFormDto.getNickName())
-                .email(userFormDto.getEmail())
-                .password(passwordEncoder.encode(userFormDto.getPassword()))
-                .address(userFormDto.getAddress())
-                .gender(userFormDto.getGender())
-                .userRole(UserRole.USER)
-                .createDate(LocalDateTime.now()).build();
+//public static User createUser(UserFormDto userFormDto, PasswordEncoder passwordEncoder) {
+//    String password = passwordEncoder.encode(userFormDto.getPassword());
+//    User user = User.builder()
+//            .name(userFormDto.getName())
+//            .nickName(userFormDto.getNickName())
+//            .email(userFormDto.getEmail())
+//            .password(password)
+//            .address(userFormDto.getAddress())
+//            .gender(userFormDto.getGender())
+//            .userRole(UserRole.USER)
+//            .createDate(LocalDateTime.now()).build();
+//}
+
+  public static User createUser(UserFormDto userFormDto, PasswordEncoder passwordEncoder){
+        String password = passwordEncoder.encode(userFormDto.getPassword());
+        User user = new User();
+        user.setName(userFormDto.getName());
+        user.setNickName(userFormDto.getNickName());
+        user.setEmail(userFormDto.getEmail());
+        user.setPassword(password);
+        user.setAddress(userFormDto.getAddress());
+        user.setCreateDate(LocalDateTime.now());
+        user.setGender(userFormDto.getGender());
+        user.setUserRole(UserRole.USER);
 
         return user;
     }
-
 
 }
