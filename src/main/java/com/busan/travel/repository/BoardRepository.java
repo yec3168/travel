178b4ad3165dbb58e04888b5ec24b482d810315b
yec3168 <yec3168@naver.com>
2024-03-10 00:08:14 +0900

    레포지토리 수정

--- src/main/java/com/busan/travel/repository/BoardRepository.java
+++ src/main/java/com/busan/travel/repository/BoardRepository.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Optional<Board> findById(Long id);
 
+    //게시판(공지) 최신순
     @Query(" SELECT b "
             + "FROM Board as b "
             + "WHERE b.noticeYn = true "
@@ -21,22 +22,25 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     )
     List<Board> findByNoticeYnTrueOrderByCreateDateDesc();
 
-
+    //게시판(자유) 최신순
     @Query(  "SELECT b "
             + "from Board as b "
             + "WHERE b.noticeYn =false "
             + "order by b.createDate desc ")
     Page<Board> findByNoticeYnFalseOrderByCreateDateDesc(Pageable pageable);
 
-    //    @Query(" SELECT b "
-//            + "FROM Board as b "
-//            + "WHERE b.noticeYn = true "
-//            + "order by b.createDate desc  union "
-//            + "SELECT b "
-//            + "from Board as b "
-//            + "WHERE b.noticeYn =false "
-//            + "order by b.createDate desc "
-//    )
-//    Page<Board> findAll(Pageable pageable);
+
     Page<Board> findAll(Pageable pageable);
+
+    //게시판 조회순 (메인 홈페이지)
+    @Query( "select b " +
+            "FROM Board as b " +
+            "ORDER BY b.view desc ")
+    List<Board> findAllByOOrderByView(Board board);
+
+    //게시판 추천순 (메인 홈페이지)
+    @Query( "select b " +
+            "FROM Board as b " +
+            "ORDER BY size(b.likeVote) desc ")
+    List<Board> findByOOrderByLikeVote(Board board);
 }
